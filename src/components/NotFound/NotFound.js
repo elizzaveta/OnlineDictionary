a3e57460300acd1a0f5bcd4da3e76c0c106243dd
elizzaveta <y.timchenko-2025@kpi.com>
2023-02-10 17:36:37 +0200

    Added 404 error page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>Oops. This page that doesn't exist.</h2>
+        </div>
+    );
+};
+
+export default NotFound;
--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ import Home from "../components/Home/Home";
 import Dictionary from "../components/Dictionary/Dictionary";
 import History from "../components/History/History";
 import About from "../components/About/About";
+import NotFound from "../components/NotFound/NotFound";
 
 export const appRoutes = [
     {
@@ -19,5 +20,9 @@ export const appRoutes = [
     {
         path: 'about',
         component: <About/>
+    },
+    {
+        path: "*",
+        component: <NotFound/>
     }
 ]
