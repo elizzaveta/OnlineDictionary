21a1b6f7c4ab8694a8792ba3f13676c02bce0314
elizzaveta <y.timchenko-2025@kpi.com>
2023-02-13 17:49:19 +0200

    Changed headings tags on About page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -4,26 +4,26 @@ import styles from "../../styles/css/About.module.css"
 const About = () => {
     return (
         <div className={styles.wrapper}>
-            <h1>About this site</h1>
-            <h2>Idea</h2>
+            <h2>About this site</h2>
+            <h4>Idea</h4>
             <p>Online Dictionary is a web page that contains English words and their meanings.</p>
             <p>This site is created for learning purposes.</p>
             <br/>
-            <h2>Features</h2>
+            <h4>Features</h4>
             <ul>
                 <li>Words' meanings</li>
                 <li>Audio of pronunciation</li>
                 <li>Search history</li>
             </ul>
             <br/>
-            <h2>Technology stack</h2>
+            <h4>Technology stack</h4>
             <p>This page is created with React.js with use of css modules.</p>
             <br/>
-            <h2>API</h2>
-            <p>Dictionary information is taken from <a href="https://dictionaryapi.dev/" target="_blank">Dictionary API</a>.</p>
+            <h4>API</h4>
+            <p>Dictionary information is taken from <a href="https://dictionaryapi.dev/" target="_blank" rel="noreferrer">Dictionary API</a>.</p>
             <br/>
-            <h2>Source Code</h2>
-            <p>Source code of this page can be found at <a href="https://github.com/elizzaveta/OnlineDictionary" target="_blank">GitHub</a>.</p>
+            <h4>Source Code</h4>
+            <p>Source code of this page can be found at <a href="https://github.com/elizzaveta/OnlineDictionary" target="_blank" rel="noreferrer">GitHub</a>.</p>
         </div>
     );
 };
